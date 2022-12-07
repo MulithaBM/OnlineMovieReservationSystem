8bb25599c885bb9fa25c51ffdb6bdb1341d14565
Mulitha Moremada <mulitha@calcey.com>
2022-12-07 23:59:06 +0530

    Controllers

---
+++ OnlineMovieReservationSystem/Controllers/MovieController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineMovieReservationSystem.Data;
+using OnlineMovieReservationSystem.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OnlineMovieReservationSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private MovieDB movieDB = new();
+
+        // GET: api/<MovieController>
+        [HttpGet]
+        public ServiceResponse<List<Movie>> Get()
+        {
+            var response = new ServiceResponse<List<Movie>>();
+
+            response.Data = movieDB.GetAllMovies();
+
+            return response;
+        }
+
+        // GET api/<MovieController>/1
+        [HttpGet("{id}")]
+        public ServiceResponse<Movie> Get(int id)
+        {
+            var response = new ServiceResponse<Movie>();
+            try
+            {
+                Movie movie = movieDB.GetSingleMovie(id);
+
+                if(movie == null)
+                {
+                    response.Data = null;
+                    response.Success= false;
+                    response.Message = "Movie not found";
+                }
+                else
+                {
+                    response.Data = movie;
+                }
+            }
+            catch (Exception e)
+            {
+                response.Success = false;
+                response.Message = e.Message;
+            }
+
+            return response;
+        }
+
+        // POST api/<MovieController>
+        [HttpPost]
+        public ServiceResponse<List<Movie>> Post(Movie movie)
+        {
+            Console.WriteLine("Add movie");
+            Console.WriteLine(movie.Name);
+            var response = new ServiceResponse<List<Movie>>();
+
+            try
+            {
+                response.Data = movieDB.AddMovie(movie);
+            }
+            catch (Exception e)
+            {
+                response.Success = false;
+                response.Message = e.Message;
+            }
+
+            return response;
+        }
+
+        // PUT api/<MovieController>/1
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MovieController>/5
+        //[HttpDelete("{id}")]
+        /*public List<Movie> Delete(int id)
+        {
+            
+        }*/
+    }
+}
---
+++ OnlineMovieReservationSystem/Data/MovieDB.cs
@@ -0,0 +1,34 @@
+﻿using OnlineMovieReservationSystem.Models;
+
+namespace OnlineMovieReservationSystem.Data
+{
+    public class MovieDB
+    {
+        private static List<Movie>? Movies = new();
+
+        public Movie GetSingleMovie(int id)
+        {
+            return Movies.FirstOrDefault(x => x.Id == id);
+        }
+
+        public List<Movie> GetAllMovies()
+        {
+            return Movies;
+        }
+        public List<Movie> AddMovie(Movie movie)
+        {
+            Movies.Add(movie);
+            Console.WriteLine(Movies.Count);
+            return Movies;
+        }
+
+        // Update movie
+
+        public List<Movie> DeleteMovie(int id)
+        {
+            Movie movie = Movies.First(x => x.Id == id);
+            Movies.Remove(movie);
+            return Movies;
+        }
+    }
+}
